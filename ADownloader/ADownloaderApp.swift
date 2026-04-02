114648ff76a54d24b5399e228870dcfb3ee9b13c
carberrychai <carberrychai@gamil.com>
2026-04-02 15:37:35 +0800

    Initial Commit

---
+++ ADownloader/ADownloaderApp.swift
@@ -0,0 +1,32 @@
+//
+//  ADownloaderApp.swift
+//  ADownloader
+//
+//  Created by Changlin on 4/2/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct ADownloaderApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
